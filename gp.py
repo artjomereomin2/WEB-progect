64f80c3b05723317f8aa97d719acc764038f55c6
Admin <exvita@yandex.ru>
2022-04-11 18:30:07 +0400

    wayFinder +--+-

--- gp.py
+++ gp.py
@@ -1,6 +1,12 @@
 import pymorphy2
 
 
+# /FindOne ... - Ищет ближайший объект, указанный после команды, оценивает время до него
+# /FindAny ...(количество) ...(объект) - Ищет ближайшие объекты, количество и тип которых указано после команды, оценивает время до них
+# /SetLocation ...(геолокация) - Задаёт начальное местоположение
+# /From ...(место откуда) to ...(место куда) - Оценивает время пути между 2 точками
+# /Text ...(предложение) - Расапознаёт запрос пользователя и сообщает куда и как долго ему нужно идти
+
 class GetPlaces:
     def __init__(self):
         self.morph = pymorphy2.MorphAnalyzer()
@@ -15,22 +21,36 @@ class GetPlaces:
                 others += c
         return (new_text.split(), others)
 
-    def where_to_go(self, text):
+    def where_to_go(self, text, command_type='/Text'):
         words, others = self.get_letters_and_etc(text)
-        nouns = []
-        for i in range(len(words)):
-            word = words[i]
-            print(self.morph.parse(word)[0].tag)
-            if "NOUN" in self.morph.parse(word)[0].tag or (
-                    "ADJF" in self.morph.parse(word)[0].tag and word[0].isupper()):
-                if i != 0 and "ADJF" in self.morph.parse(words[i - 1])[0].tag:
-                    nouns.append(
-                        f"{self.morph.parse(words[i - 1])[0].normal_form} {self.morph.parse(word)[0].normal_form}")
+        if command_type == '/FindOne':
+            return words
+        if command_type == '/FindAny':
+            return (words[0], ' '.join(words[1:]))
+        if command_type == '/From':
+            ans = [[], []]
+            k = 0
+            for word in words:
+                if word != 'to':
+                    ans[k].append(word)
                 else:
-                    nouns.append(self.morph.parse(word)[0].normal_form)
-        return nouns
+                    k = 1
+            return ans
+        if command_type == '/Text':
+            nouns = []
+            for i in range(len(words)):
+                word = words[i]
+                # print(self.morph.parse(word)[0].tag)
+                if "NOUN" in self.morph.parse(word)[0].tag or (
+                        "ADJF" in self.morph.parse(word)[0].tag and word[0].isupper()):
+                    if i != 0 and "ADJF" in self.morph.parse(words[i - 1])[0].tag:
+                        nouns.append(
+                            f"{self.morph.parse(words[i - 1])[0].normal_form} {self.morph.parse(word)[0].normal_form}")
+                    else:
+                        nouns.append(self.morph.parse(word)[0].normal_form)
+            return nouns
 
 
 example = GetPlaces()
 
-print(example.where_to_go('Было бы неплохо добраться до Красивой площади Ленина'))
+# print(example.where_to_go('Где здесь хорошая аптека к Солнечному'))
--- ws.py
+++ ws.py
@@ -1,8 +1,11 @@
 from gp import GetPlaces
 from random import randrange as r
 from random import choice
+import math
+from datetime import timedelta
 
 text_analizer = GetPlaces()
+
 
 # 2022-04-07 22:03:17,254 - telegram.ext.dispatcher - DEBUG - Processing Update: {'message': {'chat': {'username': '***', 'first_name': '***', 'type': 'private', 'id': ***}, 'message_id': 440, 'new_chat_members': [], 'new_chat_photo': [], 'entities': [], 'caption_entities': [], 'group_chat_created': False, 'supergroup_chat_created': False, 'location': {'latitude': ***, 'longitude': ***}, 'date': 1649354597, 'channel_chat_created': False, 'photo': [], 'delete_chat_photo': False, 'from': {'username': '***', 'is_bot': False, 'id': ***, 'language_code': 'ru', 'first_name': '***'}}, 'update_id': 81211740}
 
@@ -14,35 +17,106 @@ class Vertex:
         self.address = address
         self.location = location
 
-    def dist(self, other):
-        # может быть не работает
-        return ((self.location[0] - other.location[0]) ** 2 + (self.location[1] - other.location[1]) ** 2) ** 0.5
+    def get_distance(self, other):
+        # p1 и p2 - это кортежи из двух элементов - координаты точек
+        radius = 6373.0
+
+        lon1 = math.radians(self.location[0])
+        lat1 = math.radians(self.location[1])
+        lon2 = math.radians(other.location[0])
+        lat2 = math.radians(other.location[1])
+
+        d_lon = lon2 - lon1
+        d_lat = lat2 - lat1
+
+        a = math.sin(d_lat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(d_lon / 2) ** 2
+        c = 2 * math.atan2(a ** 0.5, (1 - a) ** 0.5)
+
+        distance = radius * c
+        return distance
+
+    def time(self, other):
+        return timedelta(hours=self.get_distance(other) / 4).seconds
+
+    def timeRepr(self, other):
+        res = timedelta(hours=self.get_distance(other) / 4)
+        s = str(res).split()
+        # print(s)
+        days = int(s[0])
+        hours = int(s[2].split(':')[0])
+        minutes = int(s[2].split(':')[1])
+        ans = ['Идти']
+        if days:
+            ans.append(f"{days} дней")
+        if hours:
+            ans.append(f"{hours} часов")
+        if minutes:
+            ans.append(f"{minutes} минут")
+        return ' '.join(ans)
 
 
 class WayFinder:
-    def do_work(self, text):
-        self.points = []
-        places = text_analizer.where_to_go(text)
+    def do_work(self, text, command_type):
+        self.points = {}
+        places = text_analizer.where_to_go(text, command_type)
         for place in places:
             # TODO тут нужно вписать фунцкию, которая по названию(н-р 'аптека')
             #  и кол-ву необходимых результатов ищет ближайшие(геокодером)
             #  места(их название, тип места(аптека,ресторан...), адресс и координаты)
             mid_ress = ...
             mid_ress = [(f'{place}{i}', place, f'Московская{i}', (r(1000), r(1000))) for i in range(5)]
+            self.points[place] = []
             for x in mid_ress:
-                self.points.append(Vertex(*x))
-        result = self.find(0, 0, places)  # TODO вместо 0,0 нужно вписать координаты пользователя
+                self.points[place].append(Vertex(*x))
+        # print(self.points)
+        self.start = (0, 0)  # текущие координаты пользователя
+        result = self.find('START', -1, [], places)  # TODO вместо 0,0 нужно вписать координаты пользователя
+        points = []
+        for place in result[0]:
+            points.append(f"*{place.name}({place.address})")
+        res = timedelta(seconds=result[1])
+        days = res.days
+        seconds = res.seconds
+        hours = (seconds % (3600 * 24)) // 3600
+        minutes = seconds % 60
+        ans = ['Идти']
+        if days:
+            ans.append(f"{days} дней")
+        if hours:
+            ans.append(f"{hours} часов")
+        if minutes:
+            ans.append(f"{minutes} минут")
+        line = '\n'
+        return f"Следуйте по маршруту:\n{line.join(points)}\nВремя пути {ans}"
 
-    def find(self, x0, y0, to_find, v=-1, way=[], dist=0):
-        if not to_find:
-            return dist, way
-        best_dist = 10 ** 9
+    def find(self, now_type, ind, way, to_go, time=0):
+        # print(now_type, ind, way, to_go, time)
+        if not to_go:
+            return way, time
         best_way = []
-        for i in range(10):
-            to_go_i = r(len(self.points))
-            to_go = self.points[to_go_i]
-            if to_go.type not in to_find:
-                continue
-            new_find_list = to_find.copy()
-            new_find_list.pop(new_find_list.index(to_go.type))
-            d, w = self.find(x0, y0, new_find_list, to_go_i, )  # TODO ДОДЕЛАТЬ МАРШРУТ
+        best_time = 10 ** 9
+        for _ in range(5):
+            try_type = choice(to_go)
+            try_ind = r(len(self.points[try_type]))
+            try_way = way + [self.points[try_type][try_ind]]
+            try_to_go = to_go.copy()
+            try_to_go.pop(try_to_go.index(try_type))
+            if now_type != "START":
+                try_time = time + self.points[now_type][ind].time(self.points[try_type][try_ind])
+            else:
+                try_time = time + Vertex("START", "START", "NOWHERE", self.start).time(self.points[try_type][try_ind])
+            resway, restime = self.find(try_type, try_ind, try_way, try_to_go, try_time)
+            if restime < best_time:
+                best_way = resway.copy()
+                best_time = restime
+        return best_way, best_time
+
+
+example = WayFinder()
+
+# example.do_work()
+
+# v1, v2 = Vertex("Москва", "город", "Москва", (55.755799, 37.617617)), Vertex("Питер", "город", "Питер", (59.938955, 30.315644))
+# print(v1.time(v2))
+
+print(example.do_work("Где здесь хорошая аптека к Солнечному", '/Text'))
