a6eb792f2e596225c80db8b024938c8aaaf9ed70
Admin <exvita@yandex.ru>
2022-04-11 17:20:31 +0400

    analizer details

--- gp.py
+++ gp.py
@@ -10,7 +10,7 @@ class GetPlaces:
         others = ''
         for c in text:
             if c.isalpha() or c in [' ', '\n']:
-                new_text += c.lower()
+                new_text += c
             else:
                 others += c
         return (new_text.split(), others)
@@ -18,7 +18,19 @@ class GetPlaces:
     def where_to_go(self, text):
         words, others = self.get_letters_and_etc(text)
         nouns = []
-        for word in words:
-            if "NOUN" in self.morph.parse(word)[0].tag:
-                nouns.append(self.morph.parse(word)[0].normal_form)
+        for i in range(len(words)):
+            word = words[i]
+            print(self.morph.parse(word)[0].tag)
+            if "NOUN" in self.morph.parse(word)[0].tag or (
+                    "ADJF" in self.morph.parse(word)[0].tag and word[0].isupper()):
+                if i != 0 and "ADJF" in self.morph.parse(words[i - 1])[0].tag:
+                    nouns.append(
+                        f"{self.morph.parse(words[i - 1])[0].normal_form} {self.morph.parse(word)[0].normal_form}")
+                else:
+                    nouns.append(self.morph.parse(word)[0].normal_form)
         return nouns
+
+
+example = GetPlaces()
+
+print(example.where_to_go('Было бы неплохо добраться до Красивой площади Ленина'))
